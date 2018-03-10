c25f8569b4c223a3a5c5d7a5d58055ac1aa3f768
Billy Andersson <anderssonbilly@hotmail.com>
2018-03-10 09:55:09 +0100

    Initial Commit - Added some initial unit tests.

---
+++ Lab_02-Calculator/src/tests/CalculatorTests.java
@@ -0,0 +1,51 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTests {
+	
+	@Test
+	void addTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void addOverflowTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void substractTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void substractUnderflowTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void multiplyTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void multiplyOverflowTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void divideTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void divideByZeroTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void modulusTest() {
+		fail("Not yet implemented");
+	}
+}
---
+++ Lab_02-Calculator/src/tests/EvaluatorTests.java
@@ -0,0 +1,42 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EvaluatorTests {
+
+	@Test
+	void smallExpressionTest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void longExpressionTest() {
+		fail("Not yet implemented");
+	}
+
+	
+	@Test
+	void multipleOpsTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void multipleOpsInRowTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void priorityTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void parenthesesTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void wrongNumberOfParenthesesTest() {
+		fail("Not yet implemented");
+	}
+}
