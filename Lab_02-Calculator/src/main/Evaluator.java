6db80b1a6bea0d88192fc61fa9123629d2d8b681
Billy Andersson <anderssonbilly@hotmail.com>
2018-03-11 15:40:27 +0100

    Rewrote comments in the Evaluator

--- Lab_02-Calculator/src/main/Evaluator.java
+++ Lab_02-Calculator/src/main/Evaluator.java
@@ -30,112 +30,120 @@ public class Evaluator {
 		// Stack to check for the correct number of parentheseses
 		pStack = new Stack<String>();
 
-		// Stringbuilder for builder number to be added to expression
+		// Stringbuilder for building the numbers to be added to infix
 		StringBuilder sb = new StringBuilder();
 
 		// Loop throu String expression token for token
 		for (int i = 0; i < expression.length(); i++) {
+			// Save token for easy access
 			String token = String.valueOf(expression.charAt(i));
 
+			// Parenthesese check
 			if (token.equals("(") || token.equals(")")) {
+				// If not empty and is a )
 				if (!pStack.isEmpty() && token.equals(")")) {
+					// And the top isnt a ( throw an error
 					if (!pStack.pop().equals("("))
 						throw new ParseException("Wrong number of parentheseses", 0);
 				} else if (token.equals(")"))
+					// else if trying to add ) to empty stack throw error
 					throw new ParseException("Wrong number of parentheseses", 0);
 				else {
+					// Else push to stack
 					pStack.push(token);
 				}
 			}
 
-			// If the token at the beginning is an operator
+			// If the first token in the expression is an operator
 			if (i == 0 && !IntChecker.check(token)) {
 				// And is a minus
 				if (token.equals("-"))
 					// Push to stack
 					stack.push(token);
 				else if (token.equals("("))
+					// And if it is an ( add it to infix
 					infix.add(token);
 				else
-					// Throw exception is token at begining aint a -
+					// Throw exception is token at begining is any other character
 					throw new ParseException("Expression cant begin with an operator", 0);
 			} else if (!IntChecker.check(token)) {
-				// System.out.println();
-				// System.out.println("At op check");
-				// System.out.println("token: " + token);
-				// System.out.println("Stack: " + stack.toString());
-				// System.out.println("infix: " + infix.toString());
+				// If the token is an operator
 
+				// And we got more than 2 characters in infix
 				if (infix.size() > 2) {
 					// Check if there are two operators in a row
+					// with the exception of if ) isnt at the second last and the last isnt a (
 					boolean last = IntChecker.check(infix.get(infix.size() - 1));
 					boolean nextToLast = IntChecker.check(infix.get(infix.size() - 2));
-
 					// If there are throw an exception
 					if (!last && !nextToLast && !infix.get(infix.size() - 2).equals(")")
 							&& !infix.get(infix.size() - 1).equals("("))
 						throw new ParseException("The two last in output is op - Cant have multiple operators in a row",
 								0);
 				}
-				// Check if last in infix is an operator and top of stack
+				// If the stack contains characters
 				if (!stack.empty()) {
-					// If token isnt an number and the top token on the stack
-					// pop tokens from the stack into a string
-					// If there are throw an exception
+					// If top of stack is an operator and it isnt a - or a (
+					// Throw an error
 					if (!IntChecker.check(stack.peek()) && !(stack.peek().equals("-") && token.equals("(")))
-						throw new ParseException("Top of stack op - Cant have multiple operators in a row", 0);
+						throw new ParseException("Top of stack error - Cant have multiple operators in a row", 0);
 
-					// if (!(stack.peek().equals("-") && token.equals("("))) {
+					// While there are characters in the stack, add them to SB
 					while (!stack.empty())
 						sb.insert(0, stack.pop());
 
-					// Then add that string to infix expression
+					// Then add that string to infix expression and empty the SB
 					infix.add(sb.toString());
 					sb = new StringBuilder();
-					// Add operator to infix after number
+					// Add operator to infix
 					infix.add(token);
-					// } else
-					// stack.push(token);
 				} else if (token.equals("-"))
+					// If the token is a -
+
 					// Push minus to stack to create negative numbers
+					// Only if the last character in infix isnt a )
+					// If so add it to infix instead
 					if (!infix.get(infix.size() - 1).equals(")"))
 						stack.push(token);
 					else
 						infix.add(token);
 				else if (token.equals("(")) {
+					// If the token is a (, add it to infix
 					infix.add(token);
 				} else {
+					// Else if the last character isnt a )
+					// Add it to infix otherwise throw an exception
 					if (!infix.get(infix.size() - 1).equals(")"))
 						throw new ParseException("Empty stack - Cant have multiple operators in a row", 0);
 					else
 						infix.add(token);
 				}
 			} else {
+				// If token is a number
 				if (!stack.empty()) {
+					// And stack contains characters and it is an (
 					if (stack.peek().equals("(")) {
 						while (!stack.empty())
 							sb.insert(0, stack.pop());
+						// Write stack to infix
 						infix.add(sb.toString());
 						sb = new StringBuilder();
 					}
 				}
+				// Else push number to stack
 				stack.push(token);
 			}
-
-			// System.out.println();
-			// System.out.println("At end");
-			// System.out.println("token: " + token);
-			// System.out.println("Stack: " + stack.toString());
-			// System.out.println("infix: " + infix.toString());
 		}
 
-		if(!pStack.empty())
+		// If the parenthesese stack still contains characters throw an error
+		if (!pStack.empty())
 			throw new ParseException("Wrong number of parentheseses", 0);
-		
-		// Pop the last tokens from the stack and add to the expression
+
+		// Pop the last tokens from the stack and add to the infix
 		while (!stack.empty())
 			sb.insert(0, stack.pop());
 
+		// If SB contains characters add them to infix
 		if (sb.toString().length() > 0)
 			infix.add(sb.toString());
 
